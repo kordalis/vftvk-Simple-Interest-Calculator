26f3cf1f7517d33f3fa9596171a019d490fc1592
Thanasis <kordalis@gmail.com>
2022-05-04 13:12:10 +0300

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    if (document.getElementById("principal").value <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+  }
     document.getElementById("result").innerText=interest;
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 }
